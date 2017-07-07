43bd15242349707058de175b21c8ccac8287017c
Robbie Reta <robbie.reta67@gmail.com>
2017-07-07 13:00:13 -0500

    Props

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class App extends React.Component{
   render(){
-    return (<h1>Hello World</h1> )// JSX convertido a javascript
+      let txt=this.props.txt // con una variable
+      return<h1>{txt}</h1>
+    // para llamar a una propiedad de un elemnto html osea un "prop"
+    // se hace con this.props y despues el nombre del prop en este caso "txt"
+    // JSX convertido a javascript
   }
 }
 
+App.PropTypes = {
+ txt : PropTypes.string,
+ cat : PropTypes.number.isRequired
+}
+
+App.defaultProps= {
+ txt : "this is the default text"
+}
 export default App
