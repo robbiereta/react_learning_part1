85b42c80fa0e00ffe22c0c792c35d0b5b39718a4
Robbie Reta <robbie.reta67@gmail.com>
2017-07-07 17:05:28 -0500

    Events

--- src/App.js
+++ src/App.js
@@ -4,42 +4,36 @@ import PropTypes from 'prop-types';
 class App extends React.Component{
   constructor(){
     super();
-    this.state = {
-      txt: "this is the state text"
-    }
+    this.state = { currentEvent: "---"}
+    this.update= this.update.bind(this)
   }
 
 update(e){
-  this.setState({txt: e.target.value})
+  this.setState({currentEvent: e.type})
 }
 
 
   render(){ // con una variable
-      return <button> i <Heart/> React </button>
-      // <div>
-      // <h1>{this.state.txt}</h1>
-      // <Widget update={this.update.bind(this)}/>
-      // </div>
-
-    // para llamar a una propiedad de un elemnto html osea un "prop"
-    // se hace con this.props y despues el nombre del prop en este caso "txt"
-    // JSX convertido a javascript
+      return (
+        <div>
+        <textarea
+         onKeyPress={this.update}
+         onCopy={this.update}
+         onCut={this.update}
+         onPaste={this.update}
+         onFocus={this.update}
+         onBlur={this.update}
+         onDoubleClick={this.update}
+         onTouchStart={this.update}
+         onTouchMove={this.update}
+         onTouchEnd={this.update}
+         cols="30"
+         rows="10"
+        />
+        <h1>{this.state.currentEvent}</h1>
+        </div>
+      )
   }
 } // App Component
 
-const Button = (props) => <button>{props.children}</button>
-
-class Heart extends React.Component{
-  render(){
-    return <span>&hearts;</span>
-  }
-}
-
-// const Widget = (props) =>
-// <input type="text" onChange={props.update}/>
-//
-//
-// App.defaultProps= {
-//  txt : "this is the default text"
-// }
 export default App
