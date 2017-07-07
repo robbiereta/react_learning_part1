0c4049ab2edb82483ca20aa70995544c9015a133
Robbie Reta <robbie.reta67@gmail.com>
2017-07-07 12:39:35 -0500

    good practice: poner en parentesis el return

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import React from 'react';
 
-//Componente normal de react con class
-// class App extends React.Component{
-//   render(){
-//     return <h1>Hello World</h1> // JSX convertido a javascript
-//   }
-// }
+class App extends React.Component{
+  render(){
+    return (<h1>Hello World</h1> )// JSX convertido a javascript
+  }
+}
 
-//stateless, sin state
-const App = () => <h1>This is a stateless component</h1>
 export default App
