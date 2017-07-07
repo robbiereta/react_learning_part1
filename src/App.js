76a0a93231f87278c373c8ed9a38ee2afc632aa5
Robbie Reta <robbie.reta67@gmail.com>
2017-07-07 12:31:38 -0500

    Hello world with class and stateless

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 import React from 'react';
 
-const App = () => <h1>Hi</h1>
+//Componente normal de react con class
+// class App extends React.Component{
+//   render(){
+//     return <h1>Hello World</h1> // JSX convertido a javascript
+//   }
+// }
 
+//stateless, sin state
+const App = () => <h1>This is a stateless component</h1>
 export default App
