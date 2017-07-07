d6035c866fc3bfc0cc410ac026c067710976339b
Robbie Reta <robbie.reta67@gmail.com>
2017-07-07 14:41:33 -0500

    nested components in components

--- src/App.js
+++ src/App.js
@@ -15,23 +15,31 @@ update(e){
 
 
   render(){ // con una variable
-      return(
-      <div>
-      <h1>{this.state.txt}</h1>
-      <Widget update={this.update.bind(this)}/>
-      </div>
-            )
+      return <button> i <Heart/> React </button>
+      // <div>
+      // <h1>{this.state.txt}</h1>
+      // <Widget update={this.update.bind(this)}/>
+      // </div>
+
     // para llamar a una propiedad de un elemnto html osea un "prop"
     // se hace con this.props y despues el nombre del prop en este caso "txt"
     // JSX convertido a javascript
   }
+} // App Component
+
+const Button = (props) => <button>{props.children}</button>
+
+class Heart extends React.Component{
+  render(){
+    return <span>&hearts;</span>
+  }
 }
 
-const Widget = (props) =>
-<input type="text" onChange={props.update}/>
-
-
-App.defaultProps= {
- txt : "this is the default text"
-}
+// const Widget = (props) =>
+// <input type="text" onChange={props.update}/>
+//
+//
+// App.defaultProps= {
+//  txt : "this is the default text"
+// }
 export default App
