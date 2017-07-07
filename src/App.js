e3a9655b951da23582ddd4ca0a71c3082f6721fc
Robbie Reta <robbie.reta67@gmail.com>
2017-07-07 14:11:35 -0500

    children props

--- src/App.js
+++ src/App.js
@@ -2,19 +2,34 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 class App extends React.Component{
-  render(){
-      let txt=this.props.txt // con una variable
-      return<h1>{txt}</h1>
+  constructor(){
+    super();
+    this.state = {
+      txt: "this is the state text"
+    }
+  }
+
+update(e){
+  this.setState({txt: e.target.value})
+}
+
+
+  render(){ // con una variable
+      return(
+      <div>
+      <h1>{this.state.txt}</h1>
+      <Widget update={this.update.bind(this)}/>
+      </div>
+            )
     // para llamar a una propiedad de un elemnto html osea un "prop"
     // se hace con this.props y despues el nombre del prop en este caso "txt"
     // JSX convertido a javascript
   }
 }
 
-App.PropTypes = {
- txt : PropTypes.string,
- cat : PropTypes.number.isRequired
-}
+const Widget = (props) =>
+<input type="text" onChange={props.update}/>
+
 
 App.defaultProps= {
  txt : "this is the default text"
